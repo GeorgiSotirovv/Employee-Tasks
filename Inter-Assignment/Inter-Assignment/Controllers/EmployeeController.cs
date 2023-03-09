4c160d0d1240bc3b9686a8942e15799251a6e64b
GeorgiSotirovv <tirko123456@abv.bg>
2023-03-09 16:11:03 +0200

    Add more services for Employees

--- Inter-Assignment/Inter-Assignment/Controllers/EmployeeController.cs
+++ Inter-Assignment/Inter-Assignment/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using Inter_Assignment.Services.Contracts;
+﻿using Inter_Assignment.Models.EmployeeModels;
+using Inter_Assignment.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -13,6 +14,7 @@ namespace Inter_Assignment.Controllers
             employeeService = _EmployeeService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Employees()
         {
 
@@ -20,5 +22,37 @@ namespace Inter_Assignment.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(EmployeeViewModel model)
+        {
+            try
+            {
+                await employeeService.AddEmployeeAsync(model);
+
+                return RedirectToAction(nameof(Employees));
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Something went wrong");
+
+                return View(model);
+            }
+        }
+
+        public async Task<IActionResult> RemoveEmployee(int employeeId)
+        {
+            await employeeService.RemoveEmployeeFromDatabaseAsync(employeeId);
+
+            return RedirectToAction(nameof(Employees));
+        }
+
+        //Edit button
     }
 }
--- Inter-Assignment/Inter-Assignment/Services/Contracts/ITaskService.cs
+++ Inter-Assignment/Inter-Assignment/Services/Contracts/ITaskService.cs
@@ -2,5 +2,12 @@
 {
     public interface ITaskService
     {
+        Task<IEnumerable<Models.TaskModels.TaskViewModel>> GetAllTasksAsync();
+
+        Task AddTasksAsync(Models.TaskModels.TaskViewModel model);
+
+        public void EditTasksInformation(Models.TaskModels.TaskViewModel targetAshtray);
+
+        Task RemoveTaskFromDatabaseAsync(int ashtrayId);
     }
 }
